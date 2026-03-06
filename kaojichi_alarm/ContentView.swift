7d2a713484d7d59050bc4d51b69c5a8cc4b3fd9c
Saki <ksaki0402@gmail.com>
2026-03-06 15:20:54 +0900

    Initial Commit

---
+++ kaojichi_alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kaojichi_alarm
+//
+//  Created by 神林沙希 on 2026/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kaojichi_alarm/kaojichi_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaojichi_alarmApp.swift
+//  kaojichi_alarm
+//
+//  Created by 神林沙希 on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct kaojichi_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
